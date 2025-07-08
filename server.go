18036cff9a01234dc8b5b9d33be5e4e04c5d95f3
Tomáš Vojík <vojik@wboy.cz>
2025-07-08 10:55:51 +0200

    :bug: Improve event server initialization and shutdown handling - Ensure event source is initialized before use - Update shutdown logic to handle existing server and event source - Remove redundant server restart logic in Reset method

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package eventserver
 
 import (
 	"context"
+	er "errors"
 	"github.com/roadrunner-server/errors"
 	"go.uber.org/zap"
 	"gopkg.in/antage/eventsource.v1"
@@ -17,41 +18,8 @@ func (s *Plugin) Serve() chan error {
 
 	s.log.Info("Starting event server...", zap.String("address", s.cfg.Address))
 
-	mux := http.NewServeMux()
-	mux.Handle("/events", s.es)
-	s.srv = &http.Server{Addr: s.cfg.Address, Handler: mux}
-	go func() {
-		err := s.srv.ListenAndServe()
-		if err != nil {
-			errCh <- errors.E(op, err)
-		}
-	}()
-
-	return errCh
-}
-
-func (s *Plugin) Stop(ctx context.Context) error {
-	s.log.Info("Stopping event server...")
-	s.eventId = 0
-	s.es.Close()
-	err := s.srv.Shutdown(ctx)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *Plugin) Reset() error {
-	const op = errors.Op("event_server_reset")
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	s.log.Info("Resetting event server...")
-
-	// Reset the event ID and close the existing event source
-	s.eventId = 0
-	if s.es != nil {
-		s.es.Close()
+	// Ensure everything is properly initialized
+	if s.es == nil {
 		s.es = eventsource.New(
 			eventsource.DefaultSettings(),
 			func(req *http.Request) [][]byte {
@@ -63,28 +31,46 @@ func (s *Plugin) Reset() error {
 				}
 			},
 		)
-	} else {
-		return errors.E(op, errors.Disabled, "event source is not initialized")
 	}
 
-	// Stop the existing server if it is running
-	if s.srv != nil {
-		err := s.srv.Shutdown(context.Background())
-		if err != nil {
-			return errors.E(op, err)
-		}
-	}
-
-	// Start a new server instance
 	mux := http.NewServeMux()
 	mux.Handle("/events", s.es)
 	s.srv = &http.Server{Addr: s.cfg.Address, Handler: mux}
+
 	go func() {
 		err := s.srv.ListenAndServe()
-		if err != nil {
-			return
+		if err != nil && !er.Is(err, http.ErrServerClosed) {
+			errCh <- errors.E(op, err)
 		}
 	}()
+
+	return errCh
+}
+
+func (s *Plugin) Stop(ctx context.Context) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	const op = errors.Op("event_server_stop")
+
+	s.log.Info("Stopping event server...")
+
+	// Reset event ID
+	s.eventId = 0
+
+	// Close event source if it exists
+	if s.es != nil {
+		s.es.Close()
+		s.es = nil
+	}
+
+	// Shutdown HTTP server if it exists
+	if s.srv != nil {
+		err := s.srv.Shutdown(ctx)
+		if err != nil {
+			return errors.E(op, err)
+		}
+		s.srv = nil
+	}
 
 	return nil
 }
